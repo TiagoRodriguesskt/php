9b748b491deb49f5553085639b4727cd601beb9c
Tiago Rodrigues <34645647+TiagoRodriguesskt@users.noreply.github.com>
2019-02-20 15:42:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,21 @@
 
 	echo "Hello World";
 
+	//DATE
+	echo date ("d/M/Y");
+
+	//DOIS NUMEROS
+	$n1 = 2;
+	$n2 = 2;
+
+	//QUEBRA DE LINHA
+	echo "<br />";
+
+	//SOMA DOS DOIS NUMEROS
+	$s = $n1 + $n2;
+
+	echo "<br />";
+	//SAIDA DO RESULTADO
+	echo  $s;
+
 ?>
